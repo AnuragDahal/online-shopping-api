387911b6677e74b69e1fc070ac03112694f4da98
AnuragDahal <freefirepros2020@gmail.com>
2024-02-02 15:59:22 +0545

    Delete cookie session route

--- routes/cookies.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Response, APIRouter
-
-router = APIRouter(tags=["cookie"])
-
-
-@router.post("/session/")
-def cookie(response: Response):
-    response.set_cookie(key="mysession", value="1242r")
-    return {"message": "Wanna cookie?"}
