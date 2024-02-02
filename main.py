caf91a575f3dd5540f7258e613b5173f4314963f
AnuragDahal <freefirepros2020@gmail.com>
2024-02-02 16:08:32 +0545

    Remove unused route from main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from config import models, database
-from routes import orders, users, admin, auth, cookies
+from routes import orders, users, admin, auth
 
 app = FastAPI(
     title="Shopping Cart Api",
@@ -12,4 +12,3 @@ app.include_router(orders.router)
 app.include_router(users.router)
 app.include_router(admin.router)
 app.include_router(auth.router)
-app.include_router(cookies.router)
