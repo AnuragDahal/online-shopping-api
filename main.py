e47f47253f2dc523458dc73b61785ab6e5718b61
AnuragDahal <freefirepros2020@gmail.com>
2024-02-03 21:59:00 +0545

    Refactor router and fix create_order endpoint

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 
 models.Base.metadata.create_all(bind=database.engine)
 
-app.include_router(orders.router)
-app.include_router(users.router)
-app.include_router(admin.router)
 app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(orders.router)
+app.include_router(admin.router)
