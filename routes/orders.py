2675c3dfeb73bc568ff7df3ada1b79e36660d5af
AnuragDahal <freefirepros2020@gmail.com>
2024-02-03 21:54:58 +0545

    Refactor user authentication in order routes

--- routes/orders.py
+++ routes/orders.py
@@ -49,11 +49,11 @@ async def create_order(req: schemas.Order, request: Request, db: Session = Depen
         return {"message": "An error occurred while processing the request"}
 
 # get all orders of a particular user
-#current_user: is a dependency injection or a middleware that checks if the user is logged in or not
+# current_user: is a dependency injection or a middleware that checks if the user is logged in or not
 
 
 @router.get("/user/order/{user_id}", status_code=status.HTTP_200_OK)
-async def get_user_orders(user_id: int, db: Session = Depends(database.get_db), current_user: schemas.UserLogin = Depends(oauth.get_current_user)):
+async def get_user_orders(user_id: int, db: Session = Depends(database.get_db)):
     user_data = users.is_user(user_id, db)
     if not user_data:
         return {"message": "Invalid user id please signup first"}
@@ -67,7 +67,7 @@ async def get_user_orders(user_id: int, db: Session = Depends(database.get_db), 
 
 # get status of a particular order
 @router.get("/order/status/{order_id}", response_model=schemas.OrderStatus, status_code=status.HTTP_200_OK)
-async def get_order_status(order_id: int, db: Session = Depends(database.get_db), current_user: schemas.UserLogin = Depends(oauth.get_current_user)):
+async def get_order_status(order_id: int, db: Session = Depends(database.get_db)):
     order = db.query(models.Order).filter(
         models.Order.order_id == order_id).first()
     if not order:
