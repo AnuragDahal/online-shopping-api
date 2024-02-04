21e08f182d82001449adcb143fd3f1358e353010
AnuragDahal <freefirepros2020@gmail.com>
2024-02-04 21:44:59 +0545

    Add CORS middleware to allow cross-origin requests

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from config import models, database
 from routes import orders, users, admin, auth
 from fastapi.middleware.cors import CORSMiddleware
 
+
 app = FastAPI(
     title="Shopping Cart Api",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 models.Base.metadata.create_all(bind=database.engine)
