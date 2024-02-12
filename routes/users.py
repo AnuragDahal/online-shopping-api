14e349d2bb9921137fdfb2b715fb12f3ac143253
AnuragDahal <freefirepros2020@gmail.com>
2024-02-12 08:48:16 +0545

    Add user-related API endpoints and handlers

--- routes/users.py
+++ routes/users.py
@@ -1,70 +1,46 @@
-from fastapi import APIRouter, HTTPException, status, Depends, Request
+from fastapi import APIRouter, HTTPException, status, Depends
 from settings import database
 from sqlalchemy.orm import Session
 from typing import List
-
-from models import models, schemas
-from utils import hash
+from models import schemas
 from utils.jwt_token import verify_token
+from handlers.userhandler import(
+    CREATE_USER,
+    GET_ALL_USERS,
+    GET_USER,
+    GET_USER_BY_ID
+
+)
+
 router = APIRouter(
     prefix="/users",
     tags=["Users"],
 )
 
-
-def is_user(user_id: int, db: Session = Depends(database.get_db)):
-    try:
-        user = db.query(models.User).filter(
-            models.User.user_id == user_id).first()
-        if user:
-            return user
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=str(e))
-
-
 @router.post("/Signup", status_code=status.HTTP_201_CREATED)
 async def create_user(request: schemas.UserSignup, db: Session = Depends(database.get_db)):
-
-    # Check if the email already exists
-    if db.query(models.User).filter(models.User.email == request.email).first():
-        raise HTTPException(status_code=400, detail="Email already exists")
-    hashed_password = hash.Encryption.bcrypt(request.password)
-    # Create a new user
-    new_user = models.User(
-        name=request.name, email=request.email, password=hashed_password)
-
-    # Add the new user to the database
-    db.add(new_user)
-    db.commit()
-    return {"message": "User created successfully"}
-
+    new_user = CREATE_USER(request, db)
+    return new_user
 
 @router.get("/getallusers", response_model=List[schemas.ShowAllUser], status_code=status.HTTP_200_OK)
 async def get_all_users(db: Session = Depends(database.get_db), dependencies: Session = Depends(verify_token)):
-    user = db.query(models.User).all()
-    if not user:
-        raise HTTPException(status_code=404, detail="No users found")
+    user = GET_ALL_USERS(db)
     return user
 
-# know the user_details [ for all users]
-
-
+# get user details with email
 @router.get("/getuser/{email}", response_model=schemas.ShowParticularUser, status_code=status.HTTP_200_OK)
 async def get_user(email: str, db: Session = Depends(database.get_db), dependencies: Session = Depends(verify_token)):
-    user = db.query(models.User).filter(
-        models.User.email == email).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    
+    user_details= GET_USER(email, db)
+    return user_details
 
 # get user details with user id
 
 
 @router.get("/getuserbyid/{user_id}", response_model=schemas.ShowParticularUser, status_code=status.HTTP_200_OK)
 async def get_user_by_id(user_id: int, db: Session = Depends(database.get_db), dependencies: Session = Depends(verify_token)):
-    user = db.query(models.User).filter(
-        models.User.user_id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    try:
+        user_by_id= GET_USER_BY_ID(user_id, db)
+        return user_by_id
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
