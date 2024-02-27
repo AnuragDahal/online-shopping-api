02fab73b8b1422dffc0f642b5048d338ac327c0a
AnuragDahal <freefirepros2020@gmail.com>
2024-02-27 18:25:45 +0545

    Refactor order handler functions

--- handlers/orderhandler.py
+++ handlers/orderhandler.py
@@ -8,46 +8,38 @@ from sqlalchemy.orm import joinedload
 
 
 def CREATE_ORDER(req: schemas.Order, db: Session = Depends(database.get_db)):
-    try:
-        new_order = models.Order(**req.model_dump(exclude={"product"}))
-        for items in req.product:
-            product_data = models.Products(
-                **items.model_dump(exclude={"order_id"}), order_id=new_order.order_id)
-            db.add(product_data)
-            db.commit()
-        db.add(new_order)
+    new_order = models.Order(**req.model_dump(exclude={"product"}))
+    for items in req.product:
+        product_data = models.Products(
+            **items.model_dump(exclude={"order_id"}), order_id=new_order.order_id)
+        db.add(product_data)
         db.commit()
-        return req
-    except Exception as e:
-        ErrorHandler.Error(e)
+    db.add(new_order)
+    db.commit()
+    return req
 
 
 def CANCEL_ORDER(order_id: int, request: Request, db: Session = Depends(database.get_db)):
-    try:
-        curr_user = UserHandler(request, db)
-        user_order = db.query(models.Order).filter(
-            models.Order.user_id == curr_user.user_id).all()
-        if order_id not in [order.order_id for order in user_order]:
-            ErrorHandler.Forbidden(
-                "You are not authorized to cancel this order")
-        cancel_order = db.query(models.Order).filter(
-            models.Order.order_id == order_id).first()
-        db.delete(cancel_order)
-        db.commit()
-        return {"message": "Order and associated products have been cancelled"}
-    except Exception as e:
-        ErrorHandler.Error(e)
+    curr_user = UserHandler(request, db)
+    user_order = db.query(models.Order).filter(
+        models.Order.user_id == curr_user.user_id).all()
+    if order_id not in [order.order_id for order in user_order]:
+        ErrorHandler.Forbidden(
+            "You are not authorized to cancel this order")
+    cancel_order = db.query(models.Order).filter(
+        models.Order.order_id == order_id).first()
+    db.delete(cancel_order)
+    db.commit()
+    return {"message": "Order and associated products have been cancelled"}
 
 
 def GET_USER_ORDERS(user_id: int, request: Request, db: Session = Depends(database.get_db)):
-    try:
-        current_user = UserHandler(request, db)
-        if current_user.user_id == user_id:
-            orders = db.query(models.Order).filter(
-                models.Order.user_id == user_id).all()
-            return orders
-    except Exception as e:
-        ErrorHandler.NotFound(e)
+    current_user = UserHandler(request, db)
+    if current_user.user_id == user_id:
+        orders = db.query(models.Order).filter(
+            models.Order.user_id == user_id).all()
+        return orders
+    ErrorHandler.Forbidden("You are not authorized to view this order")
 
 
 def GET_ORDERS_BY_ID(order_id: int, db: Session):
